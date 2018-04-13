ac78081782e3a02078ba7ae333c1bffb2030821d
Primarily Snark <starkey.joshua@gmail.com>
2018-04-13 16:21:13 -0700

    Add component parser.

--- src/component-parser.js
+++ src/component-parser.js
@@ -1,3 +1,47 @@
-function parseComponent() {}
+const rollableVerifier = /(\d+)d(\d+)([a-zA-Z0-9]*)$/;
+const constantVerifier = /\d+$/;
+
+const validRollableModifiers = ["kH", "kL", "kB", "kW"];
+
+function parseConstant({ sign, value }) {
+  return {
+    sign,
+    type: "constant",
+    constant: {
+      value: parseInt(value, 10)
+    }
+  };
+}
+
+function parseRollable({ sign, value }) {
+  const rollable = rollableVerifier.exec(value);
+
+  const modifier = rollable[3];
+  if (modifier && !validRollableModifiers.includes(modifier)) {
+    throw new Error("Component value contains an invalid rollable modifier");
+  }
+
+  return {
+    sign,
+    type: "rollable",
+    rollable: {
+      count: parseInt(rollable[1], 10),
+      size: parseInt(rollable[2], 10),
+      modifier: modifier || false
+    }
+  };
+}
+
+function parseComponent(component) {
+  if (rollableVerifier.test(component.value)) {
+    return parseRollable(component);
+  }
+
+  if (constantVerifier.test(component.value)) {
+    return parseConstant(component);
+  }
+
+  throw new Error("Component value is not a valid constant or rollable");
+}
 
 module.exports = parseComponent;
---
+++ test/component-parser.js
@@ -0,0 +1,90 @@
+const expect = require("expect");
+const parseComponent = require("../src/component-parser");
+
+describe("#parseComponent", function() {
+  it("should parse a component", function() {
+    expect(
+      parseComponent({
+        sign: "+",
+        value: "10"
+      })
+    ).toEqual({
+      sign: "+",
+      type: "constant",
+      constant: {
+        value: 10
+      }
+    });
+  });
+
+  it("should throw on an unclassified component", function() {
+    expect(() =>
+      parseComponent({
+        sign: "+",
+        value: "10a"
+      })
+    ).toThrow(/not a valid constant/);
+  });
+
+  describe("Rollables", function() {
+    it("should parse a rollable", function() {
+      expect(
+        parseComponent({
+          sign: "+",
+          value: "2d20"
+        })
+      ).toEqual({
+        sign: "+",
+        type: "rollable",
+        rollable: {
+          count: 2,
+          size: 20,
+          modifier: false
+        }
+      });
+    });
+
+    it("should parse a rollable with a modifier", function() {
+      expect(
+        parseComponent({
+          sign: "+",
+          value: "2d20kH"
+        })
+      ).toEqual({
+        sign: "+",
+        type: "rollable",
+        rollable: {
+          count: 2,
+          size: 20,
+          modifier: "kH"
+        }
+      });
+    });
+
+    it("should throw on a rollable with an invalid modifier", function() {
+      expect(() =>
+        parseComponent({
+          sign: "+",
+          value: "2d20fake"
+        })
+      ).toThrow(/invalid rollable modifier/);
+    });
+  });
+
+  describe("Constants", function() {
+    it("should parse a constant", function() {
+      expect(
+        parseComponent({
+          sign: "+",
+          value: "112"
+        })
+      ).toEqual({
+        sign: "+",
+        type: "constant",
+        constant: {
+          value: 112
+        }
+      });
+    });
+  });
+});
