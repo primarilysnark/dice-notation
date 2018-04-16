9f9bbffa1a8ead26dc2133ddb348c101e921b853
Primarily Snark <starkey.joshua@gmail.com>
2018-04-16 14:06:04 -0700

    Add component serializer.

--- src/component-serializer.js
+++ src/component-serializer.js
@@ -1,3 +1,20 @@
-function serializeComponent() {}
+function serializeComponent({ sign, ...component }) {
+  switch (component.type) {
+    case "rollable":
+      return {
+        sign,
+        value: `${component.rollable.count}d${component.rollable.size}${component.rollable.modifier || ""}`
+      };
+
+    case "constant":
+      return {
+        sign,
+        value: component.constant.value.toString(10)
+      };
+
+    default:
+      throw new Error("Component must be a valid serializable type");
+  }
+}
 
 module.exports = serializeComponent;
---
+++ test/component-serializer.js
@@ -0,0 +1,113 @@
+const expect = require("expect");
+const serializeComponent = require("../src/component-serializer");
+
+describe("#serializeComponent", function() {
+  it("should serialize a component", function() {
+    expect(
+      serializeComponent({
+        sign: "+",
+        type: "constant",
+        constant: {
+          value: 10
+        }
+      })
+    ).toEqual({
+      sign: "+",
+      value: "10"
+    });
+  });
+
+  it("should throw on an unclassified component", function() {
+    expect(() =>
+      serializeComponent({
+        sign: "+",
+        value: "10a"
+      })
+    ).toThrow(/must be a valid serializable type/);
+  });
+
+  describe("Rollables", function() {
+    it("should serialize a rollable", function() {
+      expect(
+        serializeComponent({
+          sign: "+",
+          type: "rollable",
+          rollable: {
+            count: 2,
+            size: 20,
+            modifier: false
+          }
+        })
+      ).toEqual({
+        sign: "+",
+        value: "2d20"
+      });
+    });
+
+    it("should serialize a negative rollable", function() {
+      expect(
+        serializeComponent({
+          sign: "-",
+          type: "rollable",
+          rollable: {
+            count: 1,
+            size: 10,
+            modifier: false
+          }
+        })
+      ).toEqual({
+        sign: "-",
+        value: "1d10"
+      });
+    });
+
+    it("should serialize a rollable with a modifier", function() {
+      expect(
+        serializeComponent({
+          sign: "+",
+          type: "rollable",
+          rollable: {
+            count: 3,
+            size: 6,
+            modifier: "kH"
+          }
+        })
+      ).toEqual({
+        sign: "+",
+        value: "3d6kH"
+      });
+    });
+  });
+
+  describe("Constants", function() {
+    it("should parse a constant", function() {
+      expect(
+        serializeComponent({
+          sign: "+",
+          type: "constant",
+          constant: {
+            value: 112
+          }
+        })
+      ).toEqual({
+        sign: "+",
+        value: "112"
+      });
+    });
+
+    it("should parse a negative constant", function() {
+      expect(
+        serializeComponent({
+          sign: "-",
+          type: "constant",
+          constant: {
+            value: 9
+          }
+        })
+      ).toEqual({
+        sign: "-",
+        value: "9"
+      });
+    });
+  });
+});
