139bfb208ec676e3531ade9442dde6ef6880ccbf
Primarily Snark <starkey.joshua@gmail.com>
2018-04-16 14:11:31 -0700

    Add expression serializer.

--- src/expression-serializer.js
+++ src/expression-serializer.js
@@ -1,3 +1,13 @@
-function serializeExpression() {}
+function serializeExpression(components) {
+  return components.reduce((expression, component) => {
+    if (expression.length === 0 && component.sign === "-") {
+      return `-${component.value}`;
+    } else if (expression.length === 0) {
+      return component.value;
+    } else {
+      return `${expression} ${component.sign} ${component.value}`;
+    }
+  }, "");
+}
 
 module.exports = serializeExpression;
---
+++ test/expression-serializer.js
@@ -0,0 +1,60 @@
+const expect = require("expect");
+const serializeExpression = require("../src/expression-serializer");
+
+describe("#serializeExpression", function() {
+  it("should serialize components into an expression", function() {
+    expect(serializeExpression([
+      {
+        sign: "+",
+        value: "first"
+      },
+      {
+        sign: "+",
+        value: "second"
+      }
+    ])).toEqual("first + second");
+  });
+
+  it("should serialize an expression with a negative sign", function() {
+    expect(serializeExpression([
+      {
+        sign: "-",
+        value: "first"
+      }
+    ])).toEqual("-first");
+  });
+
+  it("should serialize an expression with mixed signs", function() {
+    expect(serializeExpression([
+      {
+        sign: "+",
+        value: "first"
+      },
+      {
+        sign: "-",
+        value: "second"
+      }
+    ])).toEqual("first - second");
+  });
+
+  it("should serialize an expression with multiple components", function() {
+    expect(serializeExpression([
+      {
+        sign: "+",
+        value: "first"
+      },
+      {
+        sign: "-",
+        value: "second"
+      },
+      {
+        sign: "+",
+        value: "third"
+      }
+    ])).toEqual("first - second + third");
+  });
+
+  it("should serialize an empty component set", function() {
+    expect(serializeExpression([])).toEqual("");
+  });
+});
